efe4cd693669fbcf506fb8fee119d269569eacd1
Nancy Chauhan <nancychn1@gmail.com>
2020-11-02 23:09:38 +0530

    Seperate login into blueprint

--- snowflake/app.py
+++ snowflake/app.py
@@ -2,7 +2,6 @@ import json
 import re
 from datetime import datetime
 
-import requests
 from flask import Flask, redirect, request, url_for, render_template, session
 from flask_login import (
     LoginManager,
@@ -11,9 +10,9 @@ from flask_login import (
     logout_user,
     login_required)
 from markupsafe import Markup
-from oauthlib.oauth2 import WebApplicationClient
 
 from . import api, filters, settings
+from .controllers import login
 from .forms import RegistrationForm, AppreciationForm, LikeForm, CommentForm, OneOnOneForm, OneOnOneActionItemForm, \
     OneOnOneActionItemStateChange
 from .models import Appreciation, Comment, Like, Mention, OneOnOne, OneOnOneActionItem, User
@@ -23,9 +22,7 @@ app.secret_key = settings.SECRET_KEY
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login"
-
-client = WebApplicationClient(settings.GOOGLE_CLIENT_ID)
+login_manager.login_view = "login.login"
 
 
 @login_manager.user_loader
@@ -34,6 +31,8 @@ def load_user(user_id):
 
 
 app.register_blueprint(api.users.blueprint, url_prefix="/api/users")
+
+app.register_blueprint(login.blueprint, url_prefix="/login")
 
 
 @app.route("/")
@@ -100,10 +99,6 @@ def one_on_one_action_item():
         OneOnOneActionItem.create(action_item)
 
     return redirect(f'/1-on-1s/{form.one_on_one.data}')
-
-
-def get_google_provider_cfg():
-    return requests.get(settings.GOOGLE_DISCOVERY_URL).json()
 
 
 @app.route('/1-on-1s/action-items/done', methods=['POST'])
@@ -179,83 +174,6 @@ def dislike():
         return redirect(url_for('index'))
     else:
         return render_template('login.html')
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-
-    google_provider_cfg = get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
-
-    callback_url = settings.BASE_URL + url_for('callback')
-    request_uri = client.prepare_request_uri(
-        authorization_endpoint,
-        redirect_uri=callback_url,
-        scope=["openid", "email", "profile"],
-    )
-    return redirect(request_uri)
-
-
-@app.route("/login/callback")
-def callback():
-    code = request.args.get("code")
-
-    google_provider_cfg = get_google_provider_cfg()
-    token_endpoint = google_provider_cfg["token_endpoint"]
-
-    callback_url = settings.BASE_URL + url_for('callback')
-    token_url, headers, body = client.prepare_token_request(
-        token_endpoint,
-        authorization_response=request.url,
-        redirect_url=callback_url,
-        code=code
-    )
-    token_response = requests.post(
-        token_url,
-        headers=headers,
-        data=body,
-        auth=(settings.GOOGLE_CLIENT_ID, settings.GOOGLE_CLIENT_SECRET),
-    )
-
-    # Parse the tokens!
-    client.parse_request_body_response(json.dumps(token_response.json()))
-
-    # Now that you have tokens (yay) let's find and hit the URL
-    # from Google that gives you the user's profile information,
-    # including their Google profile image and email
-    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-    uri, headers, body = client.add_token(userinfo_endpoint)
-    userinfo_response = requests.get(uri, headers=headers, data=body)
-
-    # You want to make sure their email is verified.
-    # The user authenticated with Google, authorized your
-    # app, and now you've verified their email through Google!
-    if userinfo_response.json().get("email_verified"):
-        unique_id = userinfo_response.json()["sub"]
-        users_email = userinfo_response.json()["email"]
-        picture = userinfo_response.json()["picture"]
-        users_name = userinfo_response.json()["given_name"]
-
-        # Doesn't exist? Add it to the database.
-        user = User.get(unique_id)
-        if not user:
-            session['unique_id'] = unique_id
-            session['users_email'] = users_email
-            session['picture'] = picture
-            session['users_name'] = users_name
-            return redirect(url_for("register"))
-        # Create a user in your db with the information provided
-        # by Google
-
-        # Begin user session by logging the user in
-        login_user(user)
-
-        # Send user back to homepage
-        return redirect(url_for("index"))
-    else:
-        return "User email not available or not verified by Google.", 400
 
 
 @app.route('/register', methods=['GET', 'POST'])
