00d5b459533a215517b3eb358d28a616e151a591
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2021-01-10 23:53:04 +0530

    Update autocomplete API to use marshmallow

--- snowflake/controllers/api/users.py
+++ snowflake/controllers/api/users.py
@@ -1,13 +1,16 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 
-from snowflake.models import User
 from .response import bad_request
+from ...models import User
+from ...schemas.user import UserSchema
 
 blueprint = Blueprint('users', __name__)
 
+schema = UserSchema()
+
 
 @blueprint.route('/_autocomplete')
-def autocomplete_search():
+def autocomplete():
     term = request.args.get('q')
 
     if term is None:
@@ -15,8 +18,4 @@ def autocomplete_search():
 
     users = User.find_by_name_prefix(term)
 
-    return jsonify([{
-        'name': user.name,
-        'username': user.username,
-        'avatar': user.profile_pic
-    } for user in users])
+    return schema.jsonify(users, many=True)
