591eff929880b90b5adc9fab85c3ed110d8649e6
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2021-01-10 23:53:04 +0530

    Update autocomplete API to use marshmallow

--- snowflake/controllers/api/response.py
+++ snowflake/controllers/api/response.py
@@ -1,9 +1,17 @@
 from flask import jsonify
 
 
-def bad_request(message):
-    return 400, jsonify({'message': message})
+def error_body(message):
+    return jsonify({'message': message})
 
 
-def not_found():
-    return 404, jsonify({'message': "Not found"})
+def bad_request(message="Bad request"):
+    return error_body(message), 400
+
+
+def not_found(message="Not found"):
+    return error_body(message), 404
+
+
+def unauthorized(message="Unauthorized"):
+    return jsonify(message), 403
