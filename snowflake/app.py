35b621b386548f74e62f306ac001691bbd274acb
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2021-01-17 22:23:01 +0530

    Fix login required message background

--- snowflake/app.py
+++ snowflake/app.py
@@ -20,7 +20,8 @@ settings.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login.login"
-
+login_manager.login_message_category = "danger"
+login_manager.needs_refresh_message_category = "danger"
 
 @login_manager.user_loader
 def load_user(user_id):
