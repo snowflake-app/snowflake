3613f67a0b84c1e5897913575f58795841669b86
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2021-01-25 07:52:21 +0530

    Create react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './images/snow-outline.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} width={512} height={512} className="App-logo" alt="logo"/>
+      <h1>
+        Welcome to Snowflake
+      </h1>
+    </div>
+  );
+}
+
+export default App;
