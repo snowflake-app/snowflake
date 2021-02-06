8ab983261afbd43ace9781568c340a416a817a67
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2021-02-06 22:57:58 +0530

    Fix header dropdown margin

--- frontend/src/components/header/ProfileDropdownMenu.tsx
+++ frontend/src/components/header/ProfileDropdownMenu.tsx
@@ -1,37 +1,39 @@
 import {useAuth} from "../../hooks/use-auth";
 import {Link} from "react-router-dom";
 import React from "react";
+import styles from './ProfileDropdownMenu.module.css';
 
 export default function ProfileDropdownMenu() {
-    const auth = useAuth();
+  const auth = useAuth();
 
-    return <div className="navbar-item has-dropdown is-hoverable">
-        <Link to="/" className="navbar-link">Hello {auth.user?.name}</Link>
+  return (
+    <div className="navbar-item has-dropdown is-hoverable">
+      <Link to="/" className="navbar-link">Hello {auth.user?.name}</Link>
 
-        <div className="navbar-dropdown is-right">
-            <Link to="/" className="navbar-item">
-              <span className="icon">
-                <ion-icon size="large" name="person-circle-outline"/>
-              </span><span>
-                Profile
-              </span>
-            </Link>
-            <hr className="navbar-divider"/>
-            <Link to="/" className="navbar-item">
-                <span className="icon">
-                  <ion-icon size="large" name="help-circle-outline"/>
-                </span><span>
-                  Help
-                </span>
-            </Link>
-            <hr className="navbar-divider"/>
-            <Link to="/" className="navbar-item">
-                  <span className="icon">
-                    <ion-icon size="large" name="exit-outline"/>
-                  </span><span>
-                    Sign out
-                  </span>
-            </Link>
-        </div>
-    </div>;
+      <div className="navbar-dropdown is-right">
+        <Link to="/" className="navbar-item is-align-items-center">
+        <span className={`icon ${styles.icon}`}>
+          <ion-icon size="large" name="person-circle-outline"/>
+        </span><span>
+          Profile
+        </span>
+        </Link>
+        <hr className="navbar-divider"/>
+        <Link to="/" className="navbar-item is-align-items-center">
+        <span className={`icon ${styles.icon}`}>
+          <ion-icon size="large" name="help-circle-outline"/>
+        </span><span>
+          Help
+        </span>
+        </Link>
+        <hr className="navbar-divider"/>
+        <Link to="/" className="navbar-item is-align-items-center">
+        <span className={`icon ${styles.icon}`}>
+          <ion-icon size="large" name="exit-outline"/>
+        </span><span>
+          Sign out
+        </span>
+        </Link>
+      </div>
+    </div>);
 }
