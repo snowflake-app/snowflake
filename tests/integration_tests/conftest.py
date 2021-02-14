ef05fe1a1475dad006bd010d6be2f70fb8b26746
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2021-02-14 17:49:56 +0530

    Add healthcheck integration test

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -2,6 +2,6 @@ import pytest
 
 
 @pytest.fixture
-def client(flask_app):
-    with flask_app.test_client() as flask_client:
+def client(app):
+    with app.test_client() as flask_client:
         yield flask_client
